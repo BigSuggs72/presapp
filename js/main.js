c491f8a3a9bd39da9ebc48cb36f8afda9daaaec2
BigSuggs72 <msofi72@gmail.com>
2022-08-04 05:54:33 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.querySelector('button').addEventListener('click', preapi)
+
+async function preapi(){
+    const pname = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://presapi.herokuapp.com/api/${pname}`)
+        const data = await response.json()
+        console.log(data)
+
+      document.getElementById('number').innerText = data.number
+      document.getElementById('fName').innerText = data.firstname
+      document.getElementById('lname').innerText = data.lastname
+      document.getElementById('term').innerText = data.term
+      document.getElementById('party').innerText = data.party
+      document.getElementById('birthstate').innerText = data.birthstate
+      document.getElementById('facts').innerText = data.facts
+      document.getElementById('image').src = data.image
+      
+    } catch(error) {
+        console.log(error)
+    }
+}
